9c3d820776a89064d608fbc5fb6e2f5a3645ecdd
Tim Jödden <t.joedden@gmail.com>
2019-10-21 17:16:26 +0200

    secrets now using files instead of hardcoded text

--- install_realm.sh
+++ install_realm.sh
@@ -7,7 +7,7 @@ done;
 echo installing test realm and user
 
 docker exec -it auth bash -c '\
-  ./keycloak/bin/kcadm.sh config credentials --server http://localhost:8080/auth --realm master --user admin  --password=admin;\
+  ./keycloak/bin/kcadm.sh config credentials --server http://localhost:8080/auth --realm master --user "$(cat /run/secrets/keycloak_user_secret)"  --password="$(cat /run/secrets/keycloak_user_secret)";\
   ./keycloak/bin/kcadm.sh create realms -s realm=test -s enabled=true -o;\
   CID=$(./keycloak/bin/kcadm.sh create clients -r test -s clientId=testclient -s "redirectUris=[\"http://localhost:8081/*\"]" -i);\
   ./keycloak/bin/kcadm.sh get clients/$CID/installation/providers/keycloak-oidc-keycloak-json'
